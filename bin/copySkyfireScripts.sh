f0f3d485f8460f59a8bbba3b5f87b3720b1bdc38
Hieu Ta <hieu@skyfire.xyz>
2024-09-07 17:30:06 -0700

    fix copy script

--- bin/copySkyfireScripts.sh
+++ bin/copySkyfireScripts.sh
@@ -1,5 +1,5 @@
 cp -R ~/work/macbook-setup/work/skyfire/sky-services ~/work/skyfire/
 cp -R ~/work/macbook-setup/work/skyfire/sky-dashboard ~/work/skyfire/
-cp -R ~/work/macbook-setup/work/skyfire/web7 ~/work/skyfire/web7/
-cp -R ~/work/macbook-setup/work/skyfire/aida-ai ~/work/skyfire/aida-ai/
+cp -R ~/work/macbook-setup/work/skyfire/web7 ~/work/skyfire/
+cp -R ~/work/macbook-setup/work/skyfire/aida-ai ~/work/skyfire/
 
